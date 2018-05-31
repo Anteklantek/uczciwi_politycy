47d35f1967ff73b6c4bc1ba61d8026182efd3fdc
Antoni Kowalski <antonik68@gmail.com>
2018-05-31 12:56:39 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Uczciwi_politycy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Uczciwi_politycy.dir/main.c.o"
+  "Uczciwi_politycy.pdb"
+  "Uczciwi_politycy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Uczciwi_politycy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
