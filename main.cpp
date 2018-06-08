4b3ef173f5ab766eab93822a65fa43626e6cff94
Antek <antonik68@gmail.com>
2018-06-08 11:07:58 +0200

    added sanatorium

--- main.cpp
+++ main.cpp
@@ -528,7 +528,7 @@ int main() {
 
 
             for (int z = 0; z < world_size; z++) {
-                print("wysyałam release na sanatorium, z: %d", z);
+                print1("wysyałam release na sanatorium, z: %d", z);
                 pthread_mutex_lock(&lamport_lock);
                 lamport += 1;
                 pthread_mutex_unlock(&lamport_lock);
